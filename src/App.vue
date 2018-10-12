1145213557f3a2733893809644dc5e4a08b681b7
Lyt <1042634581@qq.com>
2018-10-12 11:36:56 +0800

    vue elementui管理后台

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p class="jj">click</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+$(function(){
+	$('.jj').click(function(){
+		alert(1)
+	})
+})
+</script>
+
+<style lang="scss">
+$color:red;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $color;
+  margin-top: 60px;
+}
+</style>
